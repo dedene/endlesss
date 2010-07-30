99795e25aece93fc492336a98fd9b133c7d01261
“Peter <peter@dedene.com>
2010-07-30 15:21:52 +0200

    Fixed js bug

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -36,8 +36,8 @@ function endlessScroll(){
         });      
         setTimeout("endlessScroll()",250);
         fire = false;
-      }        
-    });
+      }); 
+    }        
   } else {
     setTimeout("endlessScroll()",250);
   }
